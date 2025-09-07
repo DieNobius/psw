6232962ccb28b4dbbe05e31e048ff4cdf2c0cd52
Your Name <you@example.com>
2025-09-07 16:22:13 +0300

    Task-1 Убрал лишний код

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,20 +9,12 @@ function Header() {
 	return (
 		<div className="header__inner">
 			{/* <img className="logo" src={logo} alt="" /> */}
-			<nav className="menu">
-				<ul className="ul__list">
-					<li className="menu__item">
-						<Link className="menu__link menu__home" to="/">
-							Home
-						</Link>
-					</li>
-					<li className="menu__item">
-						<Link className="menu__link menu__characters" to="/characters">
-							Characters
-						</Link>
-					</li>
-				</ul>
-			</nav>
+			<Link className="menu__link menu__home" to="/">
+				Home
+			</Link>
+			<Link className="menu__link menu__characters" to="/characters">
+				Characters
+			</Link>
 		</div>
 	)
 }
