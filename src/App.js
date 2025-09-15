634cf39c595875a79189c4ad566e1d297674be04
Your Name <you@example.com>
2025-09-15 14:39:26 +0300

    task-1-fix переделал роутинг

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import { BrowserRouter } from 'react-router-dom'
 import AppRoutes from './AppRoute'
-import ConditionalHeader from './components/Header/ConditionalHeader/ConditionalHeader'
 import './App.css'
 
 function App() {
 	return (
 		<BrowserRouter>
-			<ConditionalHeader />
 			<AppRoutes />
 		</BrowserRouter>
 	)
