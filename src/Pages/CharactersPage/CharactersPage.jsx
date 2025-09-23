ba4cf482fc500a4c2d49f5d5dc0c486fc1ef0162
Your Name <you@example.com>
2025-09-23 13:52:54 +0300

    task-6 сделал пагинацию

--- src/Pages/CharactersPage/CharactersPage.jsx
+++ src/Pages/CharactersPage/CharactersPage.jsx
@@ -6,13 +6,27 @@ import Filter from '../../components/Filter/Filter'
 import './CharactersPage.css'
 
 function CharactersPage() {
-	const [posts, setPosts] = useState([])
+	const [allCharacters, setAllCharacters] = useState([])
+	const [currentPage, setCurrentPage] = useState(1)
+	const [charactersPerPage, setCharactersPerPage] = useState(9)
+
+	const indexOfLastCharacter = currentPage * charactersPerPage
+	const indexOfFirstCharacter = indexOfLastCharacter - charactersPerPage
+	const currentCharacters = allCharacters.slice(
+		indexOfFirstCharacter,
+		indexOfLastCharacter
+	)
+	const totalPages = Math.ceil(allCharacters.length / charactersPerPage)
+
+	const nextPage = () =>
+		currentPage < totalPages && setCurrentPage(currentPage + 1)
+	const prevPage = () => currentPage > 1 && setCurrentPage(currentPage - 1)
 
 	useEffect(() => {
 		const asyncFetchCharactreList = async () => {
 			try {
 				const result = await getCharacterList().then((res) => res.json())
-				setPosts(result)
+				setAllCharacters(result)
 			} catch (error) {
 				console.log(error)
 			}
@@ -22,9 +36,28 @@ function CharactersPage() {
 
 	return (
 		<div className="char__inner">
-			<CharLength length={posts.length} />
+			<CharLength length={allCharacters.length} />
 			<Filter />
-			<CharList characters={posts} />
+			<CharList characters={currentCharacters} />
+			<div className="pagination">
+				<button
+					className="pagination__btn"
+					onClick={prevPage}
+					disabled={currentPage === 1}
+				>
+					Назад
+				</button>
+				<span className="pagination__info">
+					Страница {currentPage} из {totalPages}
+				</span>
+				<button
+					className="pagination__btn"
+					onClick={nextPage}
+					disabled={currentPage === totalPages}
+				>
+					Вперед
+				</button>
+			</div>
 		</div>
 	)
 }
