aa54432a5befd65dca144b48d028d3f586dda0f5
Your Name <you@example.com>
2025-09-26 10:42:02 +0300

    task-7 добавил фильтр

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,7 +1,22 @@
 import './Filter.css'
 
-function Filter() {
-	return <p className="eye">colorEye: All</p>
+function Filter({ eyeColorFilter, handleEyeColorChange }) {
+	return (
+		<div className="filter">
+			<label className="filter__label">Color eye</label>
+			<select
+				value={eyeColorFilter}
+				onChange={(e) => handleEyeColorChange(e.target.value)}
+				className="filter__select"
+			>
+				<option value="all">All</option>
+				<option value="brown">brown</option>
+				<option value="red">red</option>
+				<option value="white">white</option>
+				<option value="blue">blue</option>
+			</select>
+		</div>
+	)
 }
 
 export default Filter
